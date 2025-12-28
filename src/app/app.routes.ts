4f4a4bf165e8253b4f5c8af437ebfa3625b54e8a
G00438800 <G00438800@atu.ie>
2025-12-28 11:59:11 +0000

    Edited app.routes.ts so each page loads as a standalone component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,22 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./pages/home/home.page').then(m => m.HomePage),
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'home',
-    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
-  },
-  {
-    path: 'recipe-details',
-    loadComponent: () => import('./pages/recipe-details/recipe-details.page').then( m => m.RecipeDetailsPage)
+    path: 'recipe-details/:id',
+    loadComponent: () => import('./pages/recipe-details/recipe-details.page').then(m => m.RecipeDetailsPage),
   },
   {
     path: 'favourites',
-    loadComponent: () => import('./pages/favourites/favourites.page').then( m => m.FavouritesPage)
+    loadComponent: () => import('./pages/favourites/favourites.page').then(m => m.FavouritesPage),
   },
   {
     path: 'settings',
-    loadComponent: () => import('./pages/settings/settings.page').then( m => m.SettingsPage)
+    loadComponent: () => import('./pages/settings/settings.page').then(m => m.SettingsPage),
   },
 ];
